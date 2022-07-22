76d8cc8fe8133f3595e5e82fbedc861e0dc886b8
HSGamer <huynhqtienvtag@gmail.com>
2022-07-22 11:00:45 +0700

    log call

--- Program.cs
+++ Program.cs
@@ -9,9 +9,12 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 app.MapHub<ClientHub>("/Hub");
-app.MapPost("/", async (GlobalUpdate globalUpdate, IHubContext<ClientHub> hub) => 
-    await hub.Clients.All
-        .SendAsync(globalUpdate.MethodName, globalUpdate.Message, globalUpdate.Data));
+app.MapPost("/", async (GlobalUpdate globalUpdate, IHubContext<ClientHub> hub, ILoggerFactory loggerFactory) =>
+{
+    var logger = loggerFactory.CreateLogger<GlobalUpdate>();
+    logger.LogInformation("GlobalUpdate: {GlobalUpdateMethodName} {GlobalUpdateMessage} {GlobalUpdateData}", globalUpdate.MethodName, globalUpdate.Message, globalUpdate.Data);
+    await hub.Clients.All.SendAsync(globalUpdate.MethodName, globalUpdate.Message, globalUpdate.Data);
+});
 app.MapPost("/Group", async (GroupUpdate groupUpdate, IHubContext<ClientHub> hub) => 
     await hub.Clients.Groups(groupUpdate.GroupName)
         .SendAsync(groupUpdate.MethodName, groupUpdate.Message, groupUpdate.Data));
